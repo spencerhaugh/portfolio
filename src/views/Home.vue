1dab1b920877883304549a1c20d6f8c6c1a0a2d8
Spencer Haugh <spencer.haugh@gmail.com>
2020-12-16 17:35:10 -0500

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/WAPBars-01.png">
+    <HelloWorld msg="Let's Get Radical"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style scoped>
+  img {
+    /* background-size: cover; */
+    width: 90%;
+  }
+</style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Spencer's Projects</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Projects',
+};
+</script>
+
+<style scoped>
+
+</style>
