f546bf53603a067d990c5aa19c3a63091f7897c8
Spencer Haugh <spencer.haugh@gmail.com>
2021-02-12 17:53:20 -0500

    contact page responsiveness adjustment

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -3,7 +3,8 @@
         <h1>// LET'S TALK</h1>
         <h3>
             <ul>
-                <li><i class="fas fa-envelope-square fa-2x"></i> spencer.haugh@gmail.com</li>
+                <li><i class="fas fa-envelope-square fa-2x"></i> <span> spencer.haugh@gmail.com
+                    </span></li>
                 <li><i class="fab fa-linkedin fa-2x"></i> <a href="https://linkedin.com/in/spencerhaugh" target="_blank">linkedin.com/in/spencerhaugh</a></li>
                 <li><i class="fab fa-github-square fa-2x"></i> <a href="https://github.com/spencerhaugh" target="_blank">github.com/spencerhaugh</a></li>
             </ul>
@@ -20,12 +21,31 @@ export default {
 <style scoped>
     ul {
         list-style: none;
-        margin-top: 10%;
+        width: 25%;
+        margin: 8% auto;
         padding: 0;
     }
-    @media (max-width: 650px) {
+    li {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    @media (max-width: 1224px) {
+        ul {
+            width: 40%;
+        }
+    @media (max-width: 768px) {
         .contact {
             font-size: 0.75rem;
         }
+        ul {
+            width: 50%;
+        }
+    }
+    @media (max-width: 400px) {
+        ul {
+            width: 90%;
+        }
+    }
     }
 </style>
