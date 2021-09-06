22be3f295af13048087532e8c8ec31c3cbf31619
Spencer Haugh <spencer.haugh@gmail.com>
2021-09-06 18:14:40 -0400

    Update About to current

--- src/views/About.vue
+++ src/views/About.vue
@@ -17,56 +17,72 @@
         <p>
           I am a full stack software developer with a background in Business Development and
           Marketing. I strive to build elegant solutions to common and uncommon problems with a
-          focus on intuitive, user-friendly resolutions. I excel in Javascript, and have experience
-          with React and Vue, as well as Node and Express for the backend.
-          (I dabble in Python too!)
+          focus on intuitive, user-friendly resolutions. I excel in Javascript, and have significant
+          eperience with React. I also have built with Vue, as well as Node and Express for
+          the backend. (I even dabble in Python too!)
         </p>
         <p>
-          I am one of those rare
-          developers that loves working with CSS and styling, and I am loving Sass.
-          I love building in that interaction with users, and creating great experiences.
+          I am one of those rare software
+          developers that really loves working with CSS and styling, and am experienced with
+          CSS compiliers like Sass, and using CSS-in-JS. I love making sites responsive!
+          I also really love building interactive features for users and creating great experiences.
         </p>
         <p>
-          As a patient soul, I excel in bringing a calm to the storm of business with my years of
+          I excel in bringing a calm to the storm of business with my years of
           experience in process improvement, problem-solving, and client-first solutions.
-          My background in marketing and my interst in design lend to my strength
-          in front end development, but my analytical side also loves backend work.
-          (My love of Excel spreadsheets is a dead giveaway).
+          I've put out a lot of "fires" in my time, and am quite good under pressure.
+          My background in marketing and my interest in design lend to my strengths
+          in front end development, but my analytical side also loves backend work as well.
+          (My love of Excel spreadsheets is probably a dead giveaway).
         </p>
         <p>
           I fell in love with programming because of the endless learning! I am a person
           who loves adding new skills to my knowledge base, and with code there is no end to
           knowledge! I am excited to learn from great companies and great mentors!
-          Next on my personal agenda to learn is: Docker, Typescript, and Next.
+          Next on my personal agenda to learn is: GraphQL, Typescript, and Next.
         </p>
       </section>
 
       <section id="personal">
         <h3>Ok, Really About Me</h3>
         <p>
-          But that's business, and I'm a lot more than that! My favorite hobby outside of the
+          But that's all business, and I'm a lot more than that! My favorite hobby outside of the
           office is cycling. I've raced bicycles for years, and I enjoy it so much that
           I have a fairly popular podcast about it, (The Slow Ride Podcast). I really
           enjoy creating #content.
         </p>
+        <p>I think the years of dedication to training and racing bikes, helps me tremendously
+          as a developer. As a bike racer I spend a lot of time training solo, pputting the work
+          to be my best. But, cycling is a team sport, and on race day it's knowing how to
+          work effectively with the whole team that allows us to achieve our goals.
+          Communication is critical.
+          Showing up prepared is critical.
+          Being willing to help the team is critical.
+        </p>
         <p>
           I also have a 1988 VW Vanagon that I am trying to restore, I don't know a
-          lot about cars but I'm trying to learn.
+          lot about cars but I'm trying to learn. This process also reflects in my
+          devoper life; there are always new and interesting problems with a vintage van,
+          and knowing what my resources are, and analysing the issue to break it down
+          into managable pieces is the best way to get yourself through it
+          and back on the road!
         </p>
         <p>
           Lastly, I'm still trying to get better at play guitar, but I'm only about as
-          good as I was at 15. Still, I really enjoy it!
+          good as I was at 15. Still, I really enjoy it! Dedication, and not giving
+          up on goals, even if they are challenging is a trait I cherish.
         </p>
         <p>
-          (Feel free to ask me about any of these interests, I love to talk about them!)
+          Feel free to ask me about any of these interests, I love to talk about them!
         </p>
       </section>
 
       <section id="important">
         <h3>Also:</h3>
         <p>I also <strong>*strongly*</strong> believe in equality and dismantling problematic
-          systemic infrastructure such as racism, sexism, homophobia, and the toxic enviroments
-          that enable them. I think that this is necessary and important information to put
+          systemic infrastructure such as racism, sexism, homophobia, transphobia, and the
+          toxic enviroments that enable them.
+          I think that this is necessary and important information to put
           here on the site, as any company I agree to work for will absolutely
           need to share those beliefs.
         </p>
@@ -104,11 +120,11 @@ export default {
     width: 50%;
     padding: 3%;
     margin: 1% auto;
-    background-color: rgb(247, 242, 242);
+    background-color: rgb(253, 253, 253);
     border-radius: 10px;
-    -webkit-box-shadow: 2px 2px 8px 0px rgba(70, 70, 70, 0.69);
-    -moz-box-shadow:    2px 2px 8px 0px rgba(70, 70, 70, 0.69);
-    box-shadow:         2px 2px 8px 0px rgba(70, 70, 70, 0.69);
+    -webkit-box-shadow: 4px 4px 8px 0px rgba(70, 70, 70, 0.6);
+    -moz-box-shadow:    4px 4px 8px 0px rgba(70, 70, 70, 0.6);
+    box-shadow:        4px 4px 8px 0px rgba(70, 70, 70, 0.6);
   }
   section p {
     -webkit-animation: text-focus-in 1s cubic-bezier(0.550, 0.085, 0.680, 0.530) both;
@@ -137,7 +153,8 @@ export default {
     -webkit-box-shadow: 2px 2px 8px 0px rgba(70, 70, 70, 0.69);
     -moz-box-shadow:    2px 2px 8px 0px rgba(70, 70, 70, 0.69);
     box-shadow:         2px 2px 8px 0px rgba(70, 70, 70, 0.69);
-    animation: slide-in-blurred-right 2s cubic-bezier(0.165, 0.84, 0.44, 1) 1.75s both;
+    animation: slide-in-blurred-right 3s cubic-bezier(0.165, 0.84, 0.44, 1) 1.75s both;
+    overflow: hidden;
   }
   /* ----------------------------------------------
  * Generated by Animista on 2021-9-6 12:3:56
